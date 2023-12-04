dcfe475ebfa5dcaa2193791e20b2a8fa633ec159
EmililioFdez12 <emiliofdezglld12@gmail.com>
2023-12-04 14:33:44 +0100

    Ej 3 Hecho

--- Tablas de Base de datos/Vistas/Tarea1Vistas.sql
+++ Tablas de Base de datos/Vistas/Tarea1Vistas.sql
@@ -1,5 +1,6 @@
 -- A partir de la base de datos Neptuno, crea cuatro vistas con diferentes consultas. Las que tú quieras. 
 -- 1 Consulta que filtre por más de dos campos.
+-- Los productos de la categoria 3, y su precio por unidad sea mayor a 20.
 CREATE VIEW p_categoria3_20 AS
 SELECT *
 FROM productos
@@ -18,10 +19,28 @@ SELECT *
 FROM info_importante_cliente;
 
 -- 3 Consulta que use una subconsulta.
+CREATE VIEW p_cantidad_maxima AS
 SELECT *
 FROM productos 
-WHERE id = ;
- 
+WHERE unidades_pedido = (SELECT MAX(cantidad) FROM detalles);
 
+SELECT *
+ FROM p_cantidad_maxima;
+
+ 
 -- 4 Consulta que haga join de cuatro tablas.
--- En el script debe aparecer también el enunciado de cada consulta/vista (qué va a mostrar).
+CREATE VIEW tres_joins AS
+SELECT empleados.id ID_Empleado, empleados.apellidos Apellido_Empleado, pedidos.cliente_id, pedidos.destinatario, envios.empresa, clientes.fax
+FROM empleados
+INNER JOIN pedidos ON empleados.id = pedidos.empleado_id
+INNER JOIN envios ON pedidos.cliente_id = envios.id
+INNER JOIN clientes ON pedidos.cliente_id = clientes.id
+WHERE empleados.id BETWEEN 4 AND 8
+ORDER BY empleado_id ASC;
+ 
+ SELECT *
+ FROM tres_joins;
+
+
+
+
